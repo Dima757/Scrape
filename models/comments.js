9fbbd64ba1c9414b5d42d7ab12b71b0482e0c645
Dima <dima757@hotmail.com>
2019-01-23 16:30:51 -0600

    final

---
+++ models/comments.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+
+let Schema = mongoose.Schema; 
+
+var commentSchema = new Schema({
+  
+  
+  body: String
+
+});
+
+
+var Comment = mongoose.model("Comment", commentSchema);
+
+
+module.exports = Comment;
